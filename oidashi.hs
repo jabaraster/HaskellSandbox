034ffab60e0c59b69a8b21e745655eafa991be3a
Jabara <jabaraster@gmail.com>
2012-07-18 08:38:51 +0900

    mainte.

--- oidashi.hs
+++ oidashi.hs
@@ -5,18 +5,18 @@ import Data.List
 import Data.Maybe
 import qualified Data.Map as M
 
-data MosaicEnv = MosaicEnv { tiles::(M.Map Position Tile), pixels::[Pixel] }
+data MosaicEnv = MosaicEnv { tiles::(M.Map Location Tile), pixels::[Pixel] }
                  deriving (Show, Eq, Read)
 
 data Image = Image { imageColor::Color, oid::Integer }
              deriving (Show, Eq, Read)
 
-type Position = (Int, Int)
+type Location = (Int, Int)
 
 data Tile = Tile { pixel::Pixel, image::Image }
             deriving (Show, Eq, Read)
 
-data Pixel = Pixel { position::Position, color::Color }
+data Pixel = Pixel { position::Location, color::Color }
              deriving (Show, Eq, Read)
 
 data Color = Color { red::Int, green::Int, blue::Int }
@@ -25,19 +25,49 @@ data Color = Color { red::Int, green::Int, blue::Int }
 insertTile :: MosaicEnv -> Tile -> MosaicEnv
 insertTile env t = env { tiles = M.insert (position $ pixel t) t (tiles env)}
 
-lookupTile :: MosaicEnv -> Position -> Maybe Tile
+lookupTile :: MosaicEnv -> Location -> Maybe Tile
 lookupTile env p = M.lookup p (tiles env)
 
 (/-):: Color -> Color -> Int
-a /- b = abs (red a - red b) + abs (green a - green b) + abs (blue a - blue b)
+l /- r = abs (red l - red r) + abs (green l - green r) + abs (blue l - blue r)
 
-testData = [(Pixel (x,y) (Color ((x+1)*10) ((y+1)*10) ((x+y+2)*10))) | x <- [0..3], y <- [0..3]]
+createTestData' :: [Location] -> IO [Pixel]
+createTestData' ls = do
+  cs <- f (length ls) []
+  return $ zipWith Pixel ls cs
+  where
+    f :: Int -> [Color] -> IO [Color]
+    f 0 ps = return ps
+    f i ps = do
+      r <- getStdRandom $ randomR (0, 255)
+      g <- getStdRandom $ randomR (0, 255)
+      b <- getStdRandom $ randomR (0, 255)
+      c <- return $ Color r g b
+      cs <- f (i-1) ps
+      return (c:cs)
 
-testData' = do
-  r <- getStdRandom $ randomR (0, 255)
-  g <- getStdRandom $ randomR (0, 255)
-  b <- getStdRandom $ randomR (0, 255)
-  return $ [Pixel (x,y) (Color r g b) | x <- [0..3], y <- [0..3]]
+createTestData = do
+  ls <- return [(x,y)|x<-[0..3],y<-[0..3]]
+  return $ createTestData ls
+
+testData = [
+  Pixel {position = (0,0), color = Color {red = 136, green = 39, blue = 186}}
+  ,Pixel {position = (0,1), color = Color {red = 112, green = 24, blue = 146}}
+  ,Pixel {position = (0,2), color = Color {red = 76, green = 111, blue = 6}}
+  ,Pixel {position = (0,3), color = Color {red = 136, green = 212, blue = 205}}
+  ,Pixel {position = (1,0), color = Color {red = 59, green = 33, blue = 136}}
+  ,Pixel {position = (1,1), color = Color {red = 19, green = 0, blue = 176}}
+  ,Pixel {position = (1,2), color = Color {red = 45, green = 208, blue = 169}}
+  ,Pixel {position = (1,3), color = Color {red = 25, green = 94, blue = 63}}
+  ,Pixel {position = (2,0), color = Color {red = 187, green = 27, blue = 47}}
+  ,Pixel {position = (2,1), color = Color {red = 172, green = 178, blue = 64}}
+  ,Pixel {position = (2,2), color = Color {red = 151, green = 141, blue = 84}}
+  ,Pixel {position = (2,3), color = Color {red = 132, green = 130, blue = 153}}
+  ,Pixel {position = (3,0), color = Color {red = 252, green = 179, blue = 150}}
+  ,Pixel {position = (3,1), color = Color {red = 176, green = 30, blue = 179}}
+  ,Pixel {position = (3,2), color = Color {red = 204, green = 254, blue = 40}}
+  ,Pixel {position = (3,3), color = Color {red = 66, green = 113, blue = 101}}
+  ]
 
 sortPixels :: [Pixel] -> Color -> [Pixel]
 sortPixels [] _ = []
@@ -55,14 +85,12 @@ addColor env i = let p = lookupPixel (pixels env) (imageColor i)
 
 test_lookupPixel_01 r g b = elem (lookupPixel testData (Color r g b)) testData
 test_lookupPixel_02 r g b = let c = Color r g b
-                                min = lookupPixel testData c
-                                sd = sortPixels testData c
-                                mIdx = elemIndex min sd
-                                ps = drop ((fromMaybe (-1) mIdx)+1) sd
                                 dis = color min /- c
+                                min = lookupPixel testData c
+                                -- 最近傍を除いたピクセルの配列を得る.
+                                -- この配列内の全ての要素のピクセルのcからの距離が
+                                -- 最近傍のcからの距離と等しいか大きくなくてはならない.
+                                ps  = filter (/=min) testData
   in case ps of
     [] -> error "empty list not supported."
-    otherwise -> all (\p -> (color p /- c) > dis) ps
-  where
-    remove :: [Pixel] -> Pixel -> [Pixel]
-    remove ps p = filter (/=p) ps
+    otherwise -> all (\p -> (color p /- c) >= dis) ps
